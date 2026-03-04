599eb8ae8be55e784c2708429755a4ab152d33f6
thorax-hembleecker <mremail385@gmail.com>
2026-03-04 13:31:33 -0500

    Added folder "Statistics."

---
+++ Statistics/Lab0/Lab0.R
@@ -0,0 +1,31 @@
+# Lab 0
+
+2 + 2
+10/2
+
+3 * (2+5)
+3 * 2 + 5
+4/(2*8)
+4^2 - (2+6)/(5-3)
+
+x = 3
+x
+x^2
+x+5
+
+y = c(1, 6, 5, 4)
+y
+
+mean(y)
+median(y)
+max(y)
+min(y)
+
+y^2
+sqrt(y)
+
+getwd()
+
+mydata = read.csv("energy.csv")
+mydata
+str()
